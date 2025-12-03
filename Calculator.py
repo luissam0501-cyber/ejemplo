30c9a4318bc07feb538547bf46c062b38075214a
luissam0501-cyber <luissam0501@gmail.com>
2025-12-03 07:53:41 -0600

    Add files via upload
    agregamos codigo

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def add(self, firstNumber : int, secondNumber : int):
+        print(firstNumber + secondNumber)
+
+    def substract(self, firstNumber : int, secondNumber : int):
+        print(firstNumber - secondNumber)
+
+    def multiply(self, firstNumber : int, secondNumber : int):
+        print(firstNumber*secondNumber)
+
+    def divide(self, firstNumber: int, secondNumber : int):
+        print(firstNumber/secondNumber)
+        
+       
